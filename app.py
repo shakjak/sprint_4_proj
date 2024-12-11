7d8cdd542a00cee4561247cc4d7a2b6dab29067e
shakjak <shaksleight@gmail.com>
2024-12-10 19:55:27 -0500

    Finished Notebook, and Added code to app.py file. Includes histogram and scatterplot with check box feature.

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
 import pandas as pd
 import streamlit as st
+import plotly_express as px
 
-st.title("Heading")
-st.write("Hello World")
+df = pd.read_csv('C:/Users/shaks/sprint 4 project/sprint_4_proj/vehicles_us.csv')
 
-# Step 1: load data
+df["model_year"] = df["model_year"].fillna(0).astype(int)
+df["cylinders"] = df["cylinders"].fillna(0).astype(int)
+df["odometer"] = df["odometer"].fillna(0).astype(int)
+df["is_4wd"] = df["is_4wd"].fillna(0).astype(int)
+df["date_posted"] = pd.to_datetime(df["date_posted"])
+df["type"] = df["type"].str.lower()
+df["paint_color"] = df["paint_color"].fillna('no color')
+df = df.sort_values(by="date_posted")
+df = df.reset_index(drop=True)
+company_and_model = df['model'].str.split(' ')
+df['company'] = company_and_model.apply(lambda lst: lst[0])
+df['model'] = company_and_model.apply(lambda lst: " ".join(lst[1:]))
+df['month_posted'] = df['date_posted'].dt.to_period('M')   
 
-#steep 2: data preprocessing
+fig = px.histogram(df,x='type',title='Histogram for type')
 
+st.header('Histogram Graphs')
+st.plotly_chart(fig,use_container_width=True)
 
+fig2 = px.scatter(df,x='date_posted',y='price',title='Price of Vehichiles by Date',color='type')
+checkbox = st.checkbox('Scatter plot of Vehicle Prices and Date Posted')
+
+if checkbox:
+    st.header('Scatter plot of Vehicle Prices and Date Posted')
+    st.plotly_chart(fig2)
