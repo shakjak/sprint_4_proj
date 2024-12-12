9903d6a889b8aeff56d7b01fc2bcb53ec0329919
shakjak <shaksleight@gmail.com>
2024-12-12 18:35:05 -0500

    added details to app.py and readme files

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
-# sprint_4_proj
+## sprint_4_proj
 This will be for my 4th project in my bootcamp course.
-Repository:
+
+# Repository:
+
 notebook contains the Exploritory data anaylsis about the vehicles_us data.
 streamkit contians details for servers to run on render.
 app.py runs the app so that you can interact with the data in the notebook.
 
-Description:
+# Description:
 This project contains a EDA containing a histogram and scatter plot with the data about the vehichles.
 
 That data and visuals were also represented in the app.py so that they can be uploaded to a live site where the app can be used.
 
+# How to run code:
+In a terminal use : "streamlit run app.py" to access the project.
+Or
+Use the link below to access the project.
+
+# Link
 Link for project: https://sprint-4-proj.onrender.com
--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-df = pd.read_csv('vehicles_us.csv')
+try:
+    df = pd.read_csv('vehicles_us.csv')
+except: 
+    df = pd.read_csv('https://practicum-content.s3.us-west-1.amazonaws.com/datasets/vehicles_us.csv')
 
 df["model_year"] = df["model_year"].fillna(0).astype(int)
 df["cylinders"] = df["cylinders"].fillna(0).astype(int)
@@ -21,11 +24,30 @@ df['month_posted'] = df['date_posted'].dt.to_period('M')
 fig = px.histogram(df,x='type',title='Histogram for type')
 
 st.header('Histogram Graphs')
+
 st.plotly_chart(fig,use_container_width=True)
 
+st.write("""In our data set, most of the vehicles were either, 
+         sedans, trucks, pickups, and suvs. The amount of other type 
+         of vehicle were quite minimal in comparison to the volume of 
+         the main 4 vehicle types.""")
+
 fig2 = px.scatter(df,x='date_posted',y='price',title='Price of Vehichiles by Date',color='type')
+
 checkbox = st.checkbox('Scatter plot of Vehicle Prices and Date Posted')
 
 if checkbox:
     st.header('Scatter plot of Vehicle Prices and Date Posted')
     st.plotly_chart(fig2)
+    st.write("""The data in the scatter plot reflects that the prices
+              for their inventory didn't have too much devitation. The bulk 
+             of the pricing stayed within the 5000 to 170000 dollar range.""")
+    
+st.header("Conlcusion")
+st.write("""In the data vehicles_us data, we found that the bulk of the inventory
+          was divided between 4 vehicle types. Sedan, Suv, Trucks, and Pickups.
+          
+         The averages of the price per type is,truck: 16734.89,suv: 11149.40, 
+         sedan: 6965.35, and pickup: 16057.41. Also noted in the data is that they 
+         looked to have a consitant abmount of inventory at the location at all times. 
+         This is based on the scatter plot data.""")
